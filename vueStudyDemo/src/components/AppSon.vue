17d37fdb4cf4f2c4a47ff2e0b3cdb9c139a34197
Master Hai <2372700498@qq.com>
2018-05-23 22:46:42 +0800

    提交vueDemo框架代码

---
+++ vueStudyDemo/src/components/AppSon.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{ sayhello }}</h2>
+    <button @click="writeLog"><cite>{{ title }}1</cite></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppSon',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue, MasterJ!',
+      appSonMsg: 'AppSon.vue'
+    }
+  },
+  props: [
+    'sayhello',
+    'title'
+  ],
+  methods: {
+      writeLog () {
+          console.log(this.sayhello);
+          this.$emit('listenSth',this.appSonMsg);
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  color: #42b983;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
